69e9742620a93f9a336df1e238eea62c15499b83
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 10:23:11 +0300

    Create TeamBlood.sql

---
+++ Database/Tables/TeamBlood.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE TeamBlood(
+	TBID INT NOT NULL AUTO_INCREMENT,
+    Title VARCHAR(5) NOT NULL,
+    SignID INT NOT NULL,
+	AdditionalComments TEXT,
+    
+    PRIMARY KEY(TBID),
+    FOREIGN KEY(SignID) REFERENCES Signs(SignID)
+    
+
+) ENGINE = InnoDB;
+
+
+-- could be A or B or AB or 0.
