8cd2e9ec0edfdd7a5640867f9f5c1523c8f32c31
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 10:22:32 +0300

    Update Employees.sql

--- Database/Tables/Employees.sql
+++ Database/Tables/Employees.sql
@@ -1,8 +1,8 @@
 USE BloodDonors;
 
 CREATE TABLE Employees(
-	-- AMKA is something like a big ID unique for every person.
-	AMKA BIGINT NOT NULL,
+    -- AMKA is something like a big ID unique for every person.
+    AMKA BIGINT NOT NULL,
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
     Age INT NOT NULL,
