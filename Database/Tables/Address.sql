bc4989705f3c08504b9be0054fec247abe0395a4
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 11:07:33 +0300

    Create Address.sql

---
+++ Database/Tables/Address.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Address(
+	AddressID INT NOT NULL AUTO_INCREMENT,
+    City VARCHAR(50) NOT NULL,
+    Coyntry VARCHAR(50) NOT NULL,
+    Province VARCHAR(50) NOT NULL,
+    
+    PRIMARY KEY(AddressID)
+    
+) ENGINE = InnoDB;
