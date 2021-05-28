c5324c1900085acd3168e6b0e00aaec614b8f678
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 19:39:50 +0300

    Update main.py

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -5,7 +5,6 @@ import mysql.connector
 mydb = mysql.connector.connect(host="localhost", user="vasilhs", password="bill", database ="BloodDonors", buffered=True)
 mycursor = mydb.cursor()
 
-
 # Printing all donors in the database.
 def PrintDonors():
 
@@ -25,19 +24,22 @@ def PrintUpcomingAppointments():
     for result in mycursor.stored_results():
         print(result.fetchall())
 
+def DonationsPerDonor():
+    mycursor.callproc('DonationsPerDonor')
+    for result in mycursor.stored_results():
+        print(result.fetchall())
 
 # MENU #
 print("1- View all the donors")
 print("2- View all the employees")
 print("3- View all upcoming Appointments")
-
+print("4- View all the donations per donor")
 # User will choose what he wants to see.
 input = str(input("Select a number: "))
 
 if input == '1':
      print("All Donors")
      PrintDonors()
-
 
 elif input == '2':
     print("All employees:")
@@ -47,8 +49,12 @@ elif input == '3':
     print("Upcoming Appointments")
     PrintUpcomingAppointments()
 
-
+elif input == '4':
+    print("Donations Per Donor")
+    DonationsPerDonor()
 else:
     print("Exiting...")
+
+
 
 
