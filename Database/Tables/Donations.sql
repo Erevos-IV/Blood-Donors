3fec19d36e83be59d608882754815261d3d7f2e6
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 12:28:21 +0300

    Update Donations.sql

--- Database/Tables/Donations.sql
+++ Database/Tables/Donations.sql
@@ -1,8 +1,8 @@
-   CREATE TABLE Donations(
-    DonationID INT NOT NULL AUTO_INCREMENT,
+    CREATE TABLE Donations(
+	DonationID INT NOT NULL AUTO_INCREMENT,
     EmployeeAMKA BIGINT NOT NULL,
     DonorID INT NOT NULL,
-    DateOfDonation DATE NOT NULL,
+    DateOfDonation TIMESTAMP NOT NULL,
     
     PRIMARY KEY(DonationID),
     FOREIGN KEY(DonorID) REFERENCES Donors(DonorID),
