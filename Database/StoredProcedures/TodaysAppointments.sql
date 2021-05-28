0ffe43dc26f1482a96b3fcce45df797bb28ad575
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 21:34:59 +0300

    Create TodaysAppointments.sql

---
+++ Database/StoredProcedures/TodaysAppointments.sql
@@ -0,0 +1,14 @@
+DELIMITER //
+CREATE PROCEDURE PrintTodayAppointments()
+BEGIN
+	DECLARE Todayday INT(5); 
+    SET Todayday = EXTRACT(Day FROM CURRENT_DATE);
+ SELECT * 
+ FROM Appointments
+ WHERE EXTRACT(DAY FROM AppointmentDate) = Todayday;
+END //
+DELIMITER ;
+
+CALL PrintTodayAppointments();
+
+-- This function will print all the appointments that are the same day as the day of the "call" day.
