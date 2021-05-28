927992cefdb00bd69ed2b14a9ac8b382510f0c7f
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 11:17:30 +0300

    Create Donations.sql

---
+++ Database/Tables/Donations.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Donations(
+	  DonationID INT NOT NULL AUTO_INCREMENT,
+    DonorID INT NOT NULL,
+    DateOfDonation DATE NOT NULL,
+    
+    PRIMARY KEY(DonationID),
+    FOREIGN KEY(DonorID) REFERENCES Donors(DonorID)
+) ENGINE = InnoDB;
