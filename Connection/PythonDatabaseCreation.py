69f01d2d721326967b383b0bcfed68d5e09b8a99
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 09:10:33 +0300

    Add files via upload

---
+++ Connection/PythonDatabaseCreation.py
@@ -0,0 +1,11 @@
+# imports.
+import mysql.connector
+
+# Connector.
+mydb = mysql.connector.connect(host="localhost", user="vasilhs", password="bill", database ="BloodDonors")
+mycursor = mydb.cursor()
+
+mycursor.execute("Create database BloodDonors")
+mycursor.execute("show databases")
+for db in mycursor:
+    print(db)
