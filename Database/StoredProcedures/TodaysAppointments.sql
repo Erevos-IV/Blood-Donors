5630168d20189f8307b3efc76ced71b3158c888a
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 21:37:37 +0300

    Update TodaysAppointments.sql

--- Database/StoredProcedures/TodaysAppointments.sql
+++ Database/StoredProcedures/TodaysAppointments.sql
@@ -1,14 +1,16 @@
+
 DELIMITER //
 CREATE PROCEDURE PrintTodayAppointments()
 BEGIN
-    DECLARE Todayday INT(5); 
+	DECLARE Todayday INT(5); 
     SET Todayday = EXTRACT(Day FROM CURRENT_DATE);
-    	SELECT * 
-        FROM Appointments
- 	WHERE EXTRACT(DAY FROM AppointmentDate) = Todayday;
+        SELECT app.AppointmentID, don.FirstName, don.LastName, don.Phone
+        FROM Appointments AS app INNER JOIN donors AS don USING(DonorID)
+        WHERE EXTRACT(DAY FROM AppointmentDate) = Todayday;
 END //
 DELIMITER ;
 
 CALL PrintTodayAppointments();
+
 
 -- This function will print all the appointments that are the same day as the day of the "call" date.
