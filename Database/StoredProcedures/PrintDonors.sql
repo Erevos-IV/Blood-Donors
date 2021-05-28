ba5810f5524cda5e1e8dafd132b87bd83a38e61d
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 17:04:47 +0300

    Update PrintDonors.sql

--- Database/StoredProcedures/PrintDonors.sql
+++ Database/StoredProcedures/PrintDonors.sql
@@ -1,10 +1,12 @@
-USE BloodDonors;
+USE BloodDonors; 
 
--- This will simply print all the donors.
-DELIMITER // 
+DELIMITER //
 CREATE PROCEDURE PrintDonors()
 BEGIN
-SELECT *
-FROM donors;
+	SELECT don.DonorID, CONCAT(don.FirstName, ' ', don.LastName) AS FullName, don.hone, don.Age, don.AMKA, addr.City, tea.Title, sig.Title
+	FROM donors AS don
+	INNER JOIN signs AS sig USING(SignID)
+	INNER JOIN TeamBlood AS tea USING(TBID)
+	INNER JOIN address AS addr USING(AddressID);
 END //
 DELIMITER ;
