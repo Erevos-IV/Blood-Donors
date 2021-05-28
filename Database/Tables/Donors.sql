3cc8bf94274c1e0f539b813bc95327164ba1462b
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 11:15:44 +0300

    Update Donors.sql

--- Database/Tables/Donors.sql
+++ Database/Tables/Donors.sql
@@ -1,12 +1,16 @@
-CREATE TABLE Donors( 
-    DonorID INT NOT NULL AUTO_INCREMENT,
+
+CREATE TABLE Donors(
+	DonorID INT NOT NULL AUTO_INCREMENT,
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
     Phone VARCHAR(15) NOT NULL,
+    Age INT NOT NULL,
     AMKA BIGINT NOT NULL,
     AddressID INT NOT NULL,
     TBID INT NOT NULL,
     SignID INT NOT NULL,
+    
+    CHECK (Age >= 18 AND Age <= 70),
     
     PRIMARY KEY(DonorID),
     FOREIGN KEY(AddressID) REFERENCES Address(AddressID),
