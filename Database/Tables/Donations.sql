10742879831e2871d16ec6db889f70f9ccbee2c5
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 11:21:32 +0300

    Update Donations.sql

--- Database/Tables/Donations.sql
+++ Database/Tables/Donations.sql
@@ -1,8 +1,14 @@
+
+
+    
 CREATE TABLE Donations(
-    DonationID INT NOT NULL AUTO_INCREMENT,
+	DonationID INT NOT NULL AUTO_INCREMENT,
+    EmployeeAMKA BIGINT NOT NULL,
     DonorID INT NOT NULL,
     DateOfDonation DATE NOT NULL,
     
     PRIMARY KEY(DonationID),
-    FOREIGN KEY(DonorID) REFERENCES Donors(DonorID)
+    FOREIGN KEY(DonorID) REFERENCES Donors(DonorID),
+    FOREIGN KEY(EmployeeAMKA) REFERENCES Employees(AMKA)
+    
 ) ENGINE = InnoDB;
