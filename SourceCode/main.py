1858e7d30430a8c6f46df26e23b9c2605cbf71ad
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 19:19:18 +0300

    Update main.py

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -19,9 +19,17 @@ def PrintEmployees():
     for result in mycursor.stored_results():
         print(result.fetchall())
 
+def PrintUpcomingAppointments():
+
+    mycursor.callproc('PrintUpcomingAppointments')
+    for result in mycursor.stored_results():
+        print(result.fetchall())
+
+
 # MENU #
 print("1- View all the donors")
 print("2- View all the employees")
+print("3- View all upcoming Appointments")
 
 # User will choose what he wants to see.
 input = str(input("Select a number: "))
@@ -34,6 +42,11 @@ if input == '1':
 elif input == '2':
     print("All employees:")
     PrintEmployees()
+
+elif input == '3':
+    print("Upcoming Appointments")
+    PrintUpcomingAppointments()
+
 
 else:
     print("Exiting...")
