e96e43a07d30302c1ed134d1e3a4dc662ae76357
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 18:37:25 +0300

    Update main.py

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -11,10 +11,20 @@ def PrintDonors():
 
      mycursor.callproc('PrintDonors')
      for result in mycursor.stored_results():
+         print(result.fetchall())
+
+def PrintEmployees():
+
+    mycursor.callproc('PrintEmployees')
+    for result in mycursor.stored_results():
         print(result.fetchall())
+
+# MENU #
 
 # User will choose what he wants to see.
 input = int(input("Select a number: "))
 if input == 1:
     PrintDonors()
 
+if input == 2:
+    PrintEmployees()
