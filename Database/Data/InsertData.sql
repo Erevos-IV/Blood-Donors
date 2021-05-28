2f73984485286d8ebbae07d74ea199bbe66dae89
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-28 12:14:22 +0300

    Update InsertData.sql

--- Database/Data/InsertData.sql
+++ Database/Data/InsertData.sql
@@ -1,1 +1,35 @@
+-- Sign table
+INSERT INTO Signs VALUES(1, "Positive");
+INSERT INTO Signs VALUES(2, "Negative");
 
+-- TeamBlood table
+INSERT INTO TeamBlood VALUES(1, "A", 1, null);
+INSERT INTO TeamBlood VALUES(2, "A", 2, null);
+INSERT INTO TeamBlood VALUES(3, "B", 1, null);
+INSERT INTO TeamBlood VALUES(4, "B", 2, null);
+INSERT INTO TeamBlood VALUES(5, "AB", 1, null);
+INSERT INTO TeamBlood VALUES(6, "AB", 2, null);
+INSERT INTO TeamBlood VALUES(7, "0", 1, null);
+INSERT INTO TeamBlood VALUES(8, "0", 2, null);
+
+-- Address table
+INSERT INTO Address VALUES(1, "Peiraias", "Greece", "Korydallos");
+INSERT INTO Address VALUES(2, "Peiraias", "Greece", "Kaminia");
+INSERT INTO Address VALUES(3, "Peiraias", "Greece", "Neapolh");
+INSERT INTO Address VALUES(4, "Peiraias", "Greece", "Nikaia");
+INSERT INTO Address VALUES(5, "Peiraias", "Greece", "Drapetswna");
+INSERT INTO Address VALUES(6, "Athens", "Greece", "Agia Varvara");
+INSERT INTO Address VALUES(7, "Athens", "Greece", "Menidi");
+INSERT INTO Address VALUES(8, "Athens", "Greece", "Peristeri");
+INSERT INTO Address VALUES(9, "Athens", "Greece", "Syntagma");
+INSERT INTO Address VALUES(10, "Athens", "Greece", "Ilion");
+
+-- Employees table
+INSERT INTO Employees VALUES(2002190520, "Vasileios", "Goysetis", 19, 2019482944, "Vasilisxr5000@gmail.com", "2019-04-19 12:33:01", 60500); 
+INSERT INTO Employees VALUES(2002148129, "Kalliopi", "Papapetrou", 28, 1846291740, "Kalopa@gmail.com", "2019-03-01 06:17:30", 70100); 
+INSERT INTO Employees VALUES(2002111040, "Christiana", "Papia", 39, 1648254826, "duckgoeswah38@gmail.com", "2020-01-08 20:46:11", 60500); 
+INSERT INTO Employees VALUES(2002199274, "Kwnstantinos", "Katakoyzinos", 26, 2105729483, "Kwstakhsk@hotmail.com", "2019-12-27 18:34:30", 35500); 
+INSERT INTO Employees VALUES(2002150392, "Theodwra", "Lafazanh", 24, 2103471937, "laftheo5@laf.gr", "2020-09-19 09:13:01", 44028); 
+INSERT INTO Employees VALUES(2002162926, "Lewnidas", "Thermopylas", 31, 6937195739, "fightLewn@gmail.com", "2021-02-20 12:00:00", 99140); 
+INSERT INTO Employees VALUES(2002131739, "Klewpatra", "Erhmoy", 42, 8104629472, "Throneismine@proton.prt", "2019-07-10 19:24:20", 60500);
+ 
