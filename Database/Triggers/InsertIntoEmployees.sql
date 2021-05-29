48c0a23ef4a77f5d3d02585c1d4fb8bf262cfd18
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-29 10:06:34 +0300

    Create InsertIntoEmployees.sql

---
+++ Database/Triggers/InsertIntoEmployees.sql
@@ -0,0 +1,22 @@
+-- Creating trigger to catch any insert into employees.
+
+DELIMITER //
+CREATE TRIGGER Employee_TR_CatchingInserts
+AFTER INSERT ON employees
+FOR EACH ROW
+	BEGIN
+	DECLARE changes VARCHAR(200);
+        
+        SET changes = CONCAT(NEW.AMKA,', ',
+			                       NEW.FirstName,', ',
+                             NEW.LastName,', ',
+                             NEW.Age,', ',
+                             NEW.Phone,', ',
+                             NEW.Email,', ',
+                             NEW.HireDate,', ',
+                             NEW.Salary, ', ');
+                             
+	INSERT INTO EmployeeChanges
+        VALUES (CURRENT_USER(), 'INSERT', changes, CURRENT_TIMESTAMP());
+	END //
+DELIMITER ;
