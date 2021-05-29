6aeebafaa71924a431d0158df0198988955b97af
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-29 10:09:30 +0300

    Create DeleteOnEmployees.sql

---
+++ Database/Triggers/DeleteOnEmployees.sql
@@ -0,0 +1,22 @@
+-- Will update the table employeeschanges with some valuable information.
+
+DELIMITER //
+CREATE TRIGGER Employee_TR_CatchingDeletes
+AFTER DELETE ON employees
+FOR EACH ROW
+	BEGIN
+	DECLARE changes VARCHAR(200);
+        
+        SET changes = CONCAT(OLD.AMKA,', ',
+			                 OLD.FirstName,', ',
+                             OLD.LastName,', ',
+                             OLD.Age,', ',
+                             OLD.Phone,', ',
+                             OLD.Email,', ',
+                             OLD.HireDate,', ',
+                             OLD.Salary, ', ');
+                             
+	INSERT INTO EmployeeChanges
+        VALUES (CURRENT_USER(), 'DELETE', changes, CURRENT_TIMESTAMP());
+	END //
+DELIMITER ;
