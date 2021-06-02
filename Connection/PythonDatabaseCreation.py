bc18b0a64595ccc585f7ac705d1a2d31f028e84e
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-06-02 08:45:25 +0300

    Update PythonDatabaseCreation.py

--- Connection/PythonDatabaseCreation.py
+++ Connection/PythonDatabaseCreation.py
@@ -5,5 +5,3 @@ import mysql.connector
 mydb = mysql.connector.connect(host="localhost", user="vasilhs", password="bill", database ="BloodDonors")
 mycursor = mydb.cursor()
 
-mycursor.execute("Create database BloodDonors")
-mycursor.execute("show databases")
