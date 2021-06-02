ca70962cc41755ddc41f463bae9b8c3dd9522c9a
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-06-02 12:19:48 +0300

    Create UpdateEmployees.sql

---
+++ Database/Triggers/UpdateEmployees.sql
@@ -0,0 +1,30 @@
+DELIMITER //
+CREATE TRIGGER Employee_TR_Update
+AFTER UPDATE ON employees
+FOR EACH ROW
+	BEGIN
+	DECLARE changes VARCHAR(200);
+	DECLARE beforechanges VARCHAR(200);
+		set beforechanges = CONCAT(OLD.AMKA,', ',
+		                           OLD.FirstName,', ',
+                               OLD.LastName,', ',
+                               OLD.Age,', ',
+                               OLD.Phone,', ',
+                               OLD.Email,', ',
+                               OLD.HireDate,', ',
+                               OLD.Salary);
+                             
+        SET changes = CONCAT(NEW.AMKA,', ',
+		                         NEW.FirstName,', ',
+                             NEW.LastName,', ',
+                             NEW.Age,', ',
+                             NEW.Phone,', ',
+                             NEW.Email,', ',
+                             NEW.HireDate,', ',
+                             NEW.Salary, ', ');
+                             
+	INSERT INTO EmployeeChanges
+        VALUES (CURRENT_USER(), 'UPDATE', beforechanges, changes, CURRENT_TIMESTAMP());
+        
+	END //
+DELIMITER ;
